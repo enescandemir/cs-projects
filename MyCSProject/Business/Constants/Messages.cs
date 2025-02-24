1a441a95060e5b192621771735a432dd03a2c33c
Bilal Enes Candemir <123306660+enescandemir@users.noreply.github.com>
2025-02-24 10:09:07 +0300

    Feature

---
+++ MyCSProject/Business/Constants/Messages.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Constants // core katmanında olmamasının sebebi sadece her projede ürünler olmaz. evrensel değil.
+{
+    public static class Messages // newlenemez
+    {
+        public static string ProductAdded = "Ürün Eklendi";
+        public static string ProductNameInvalid = "Ürün İsmi Geçersiz";
+        public static string MaintenanceTime = "Bakım Zamanı";
+        public static string ProductsListed = "Ürünler Başarıyla Listelendi";
+    }
+}
